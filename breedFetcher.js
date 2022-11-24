63251ef5d7b48b4ba64deae9b8babec84b1cceb0
James Brown <jamesraymondbrown@gmail.com>
2022-11-24 17:43:37 +0000

    Finished Breed Fetcher Program

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,16 @@
 let input = process.argv.slice(2);
 
-const fs = require('fs');
-
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  // console.log(typeof body);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${input[0]}`, (error, response, body) => {
+  if (error !== null) {
+    console.log("there was an error with the program");
+    return;
+  }
   const data = JSON.parse(body);
-  //console.log(data);
-  console.log(typeof data);
-  console.log(data[0].description);
+  if (data[0] === undefined) {
+    console.log("Breed not found");
+  } else {
+    console.log(data[0].description);
+  }
 });
